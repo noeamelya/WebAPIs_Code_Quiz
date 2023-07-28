f470bf291371f0c0cc754ba790277ee6d4a9594c
noeamelya <noeamelyaofficial@hotmail.com>
2023-07-28 19:03:24 +0100

    Added list of questions

---
+++ questions.js
@@ -0,0 +1,47 @@
+// variable of list of question, choices and answers
+var questions = [{
+    question: "Inside which HTML element do we put the JavaScript?",
+    choices: ["<js>", "<script>", "<javascript>", "scripting"],
+    answer: "<script"
+},
+{question: "Where is the correct place to insert a Javascript?",
+choices: ["The <head> section", "Both the <head> section and the <body> section are correct", "The <body>section"],
+answer: "The <body> section"
+},
+{
+    question: "Inside which HTML element do we put the JavaScript?",
+    choices: ["<js>", "<script>", "<javascript>", "scripting"],
+    answer: "<script"
+},
+{question: "Where is the correct place to insert a Javascript?",
+choices: ["The <head> section", "Both the <head> section and the <body> section are correct", "The <body>section"],
+answer: "The <body> section"
+},
+{
+    question: "Inside which HTML element do we put the JavaScript?",
+    choices: ["<js>", "<script>", "<javascript>", "scripting"],
+    answer: "<script"
+},
+{question: "Where is the correct place to insert a Javascript?",
+choices: ["The <head> section", "Both the <head> section and the <body> section are correct", "The <body>section"],
+answer: "The <body> section"
+},
+{
+    question: "Inside which HTML element do we put the JavaScript?",
+    choices: ["<js>", "<script>", "<javascript>", "scripting"],
+    answer: "<script"
+},
+{question: "Where is the correct place to insert a Javascript?",
+choices: ["The <head> section", "Both the <head> section and the <body> section are correct", "The <body>section"],
+answer: "The <body> section"
+},
+{
+    question: "Inside which HTML element do we put the JavaScript?",
+    choices: ["<js>", "<script>", "<javascript>", "scripting"],
+    answer: "<script"
+},
+{question: "Where is the correct place to insert a Javascript?",
+choices: ["The <head> section", "Both the <head> section and the <body> section are correct", "The <body>section"],
+answer: "The <body> section"
+},
+]
