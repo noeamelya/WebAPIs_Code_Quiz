328ffb8808383047e683f1c6a68b6d97a89be6f5
noeamelya <noeamelyaofficial@hotmail.com>
2023-07-28 19:04:40 +0100

    started work with DOM elements

---
+++ script.js
@@ -0,0 +1,11 @@
+// DOM Elements
+
+var timerEl = document.querySelector("#time");
+var startButton = document.querySelector("#start");
+var questionEl = document.querySelector("#questions");
+var choicesEl = document.querySelector("#choices");
+var initilsInput = document.querySelector("#initials");
+var feedbackEl = document.querySelector("#feedback");
+
+
+
