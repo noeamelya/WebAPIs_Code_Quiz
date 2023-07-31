1479773911bcdfe2be7516cf91e1516efdcd6a24
noeamelya <noeamelyaofficial@hotmail.com>
2023-07-31 15:13:33 +0100

    Add question selections

--- questions.js
+++ questions.js
@@ -8,40 +8,53 @@ var questions = [{
 choices: ["The <head> section", "Both the <head> section and the <body> section are correct", "The <body>section"],
 answer: "The <body> section"
 },
-{
-    question: "Inside which HTML element do we put the JavaScript?",
-    choices: ["<js>", "<script>", "<javascript>", "scripting"],
-    answer: "<script"
+{question: "The external JavaScript file must contain the <script> tag?",
+choices: ["True", "False"],
+answer: "False"
 },
-{question: "Where is the correct place to insert a Javascript?",
-choices: ["The <head> section", "Both the <head> section and the <body> section are correct", "The <body>section"],
-answer: "The <body> section"
+{question: "Which one is a looping structure in JavaScript?",
+choices: ["All the below", "For", "While", "do-while loops"],
+answer: "All the below"
 },
-{
-    question: "Inside which HTML element do we put the JavaScript?",
-    choices: ["<js>", "<script>", "<javascript>", "scripting"],
-    answer: "<script"
+{question: "How do you create a function in JavaScript?",
+choices: ["function myFunction()", "function:myFunction", "function = myFunction"],
+answer: "function myFunction()"
 },
-{question: "Where is the correct place to insert a Javascript?",
-choices: ["The <head> section", "Both the <head> section and the <body> section are correct", "The <body>section"],
-answer: "The <body> section"
+{question: "How to write an IF statement inJavaScrip?",
+choices: ["if(i == 5)", "if i = 5", "if i == 5 then", "if i = 5 then"],
+answer: "if(i == 5)"
 },
 {
-    question: "Inside which HTML element do we put the JavaScript?",
-    choices: ["<js>", "<script>", "<javascript>", "scripting"],
-    answer: "<script"
+question: "How does a WHILE loop start?",
+choices: ["while(i <= 10)", "whilei = 1 to 10", "while(i<= 10; i++)"],
+answer: "while(i <= 10)"
 },
-{question: "Where is the correct place to insert a Javascript?",
-choices: ["The <head> section", "Both the <head> section and the <body> section are correct", "The <body>section"],
-answer: "The <body> section"
+{question: "String values must be enclosed within ____ when being assigned to variables.",
+choices: ["commas", "curly brackets", "quotes", "parentheses"],
+answer: "quotes"
 },
-{
-    question: "Inside which HTML element do we put the JavaScript?",
-    choices: ["<js>", "<script>", "<javascript>", "scripting"],
-    answer: "<script"
+{question: "How do you round the number 7,5. to the nearest integer?",
+choices: ["Math.round(7,5)", "Math.rnd(7,5)", "round(7,5)", "rnd(7,5)"],
+answer: "Math.round(7,5)"
 },
-{question: "Where is the correct place to insert a Javascript?",
-choices: ["The <head> section", "Both the <head> section and the <body> section are correct", "The <body>section"],
-answer: "The <body> section"
+{question: "JavaScript is the same as Java.",
+choices: ["False", "True"],
+answer: "False"
 },
-]
+{question: "Which event occurs when the user clicks on an HTML eelement?",
+choices: ["onclick", "onchange", "onmouseclick", "onmouseover"],
+answer: "onclick"
+},
+{question: "The condition in an if / else statement is enclosed within ____.",
+choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+answer: "parentheses"
+},
+{question: "Is JavaScript case-sensitive?",
+choices: ["Yes", "No"],
+answer: "Yes"
+},
+{question: "What will the following code return : Boolean (10>9)?",
+choices: ["True", "False", "Nan"],
+answer: "True"
+},
+];
